f04ce6ddf5157d8d9ab1b739aea563d1597a9cec
FabKo2001 <fabian-01@gmx.de>
2025-05-14 15:19:01 +0200

    predict_probability.py hinzugefügt

--- predict_probability.py
+++ predict_probability.py
@@ -6,19 +6,16 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from PIL import Image
-from torchvision import transforms
-
+import matplotlib.pyplot as plt
+import cv2
 from utils.data_loading import BasicDataset
 from unet import UNet
-from utils.utils import plot_img_and_mask
 
-import matplotlib.pyplot as plt
 
 def predict_img(net,
                 full_img,
                 device,
-                scale_factor=1,
-                out_threshold=0.5):
+                scale_factor=1):
     net.eval()
     img = torch.from_numpy(BasicDataset.preprocess(None, full_img, scale_factor, is_mask=False))
     img = img.unsqueeze(0)
@@ -27,30 +24,43 @@ def predict_img(net,
     with torch.no_grad():
         output = net(img).cpu()
         output = F.interpolate(output, (full_img.size[1], full_img.size[0]), mode='bilinear')
-        if net.n_classes > 1:
-            mask = output.argmax(dim=1)
-        else:
-            mask = torch.sigmoid(output) > out_threshold
 
-    return mask[0].long().squeeze().numpy()
+        if net.n_classes == 1:
+            probs = torch.sigmoid(output)
+        else:
+            probs = F.softmax(output, dim=1)
+
+    return probs.squeeze(0).numpy()  # Shape: [C, H, W]
+
+
+def save_probability_map(probabilities, out_filename_prefix):
+    """
+    Speichert für jede Klasse eine Heatmap der Wahrscheinlichkeiten.
+    """
+    num_classes, height, width = probabilities.shape
+    for c in range(num_classes):
+        plt.imshow(probabilities[c], cmap='viridis')
+        plt.colorbar()
+        plt.title(f'Class {c} Probability Map')
+        plt.axis('off')
+        plt.savefig(f'{out_filename_prefix}_class{c}_prob.png')
+        plt.close()
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Predict masks from input images')
+    parser = argparse.ArgumentParser(description='Predict probability maps from input images')
     parser.add_argument('--model', '-m', default='MODEL.pth', metavar='FILE',
                         help='Specify the file in which the model is stored')
     parser.add_argument('--input', '-i', metavar='INPUT', nargs='+', help='Filenames of input images', required=True)
     parser.add_argument('--output', '-o', metavar='OUTPUT', nargs='+', help='Filenames of output images')
     parser.add_argument('--viz', '-v', action='store_true',
                         help='Visualize the images as they are processed')
-    parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
-    parser.add_argument('--mask-threshold', '-t', type=float, default=0.5,
-                        help='Minimum probability value to consider a mask pixel white')
+    parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output maps')
     parser.add_argument('--scale', '-s', type=float, default=0.5,
                         help='Scale factor for the input images')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
     parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
-    
+
     return parser.parse_args()
 
 
@@ -59,23 +69,6 @@ def get_output_filenames(args):
         return f'{os.path.splitext(fn)[0]}_OUT.png'
 
     return args.output or list(map(_generate_name, args.input))
-
-
-def mask_to_image(mask: np.ndarray, mask_values):
-    if isinstance(mask_values[0], list):
-        out = np.zeros((mask.shape[-2], mask.shape[-1], len(mask_values[0])), dtype=np.uint8)
-    elif mask_values == [0, 1]:
-        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=bool)
-    else:
-        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=np.uint8)
-
-    if mask.ndim == 3:
-        mask = np.argmax(mask, axis=0)
-
-    for i, v in enumerate(mask_values):
-        out[mask == i] = v
-
-    return Image.fromarray(out)
 
 
 if __name__ == '__main__':
@@ -88,12 +81,17 @@ if __name__ == '__main__':
     net = UNet(n_channels=1, n_classes=args.classes, bilinear=args.bilinear)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     logging.info(f'Loading model {args.model}')
     logging.info(f'Using device {device}')
 
     net.to(device=device)
     state_dict = torch.load(args.model, map_location=device)
-    mask_values = state_dict.pop('mask_values', [0, 1])
+
+    # Entferne unerwarteten Schlüssel
+    if 'mask_values' in state_dict:
+        del state_dict['mask_values']
+
     net.load_state_dict(state_dict)
 
     logging.info('Model loaded!')
@@ -102,18 +100,27 @@ if __name__ == '__main__':
         logging.info(f'Predicting image {filename} ...')
         img = Image.open(filename)
 
-        mask = predict_img(net=net,
-                           full_img=img,
-                           scale_factor=args.scale,
-                           out_threshold=args.mask_threshold,
-                           device=device)
+        probs = predict_img(net=net,
+                            full_img=img,
+                            scale_factor=args.scale,
+                            device=device)
 
         if not args.no_save:
-            out_filename = out_files[i]
-            result = mask_to_image(mask, mask_values)
-            result.save(out_filename)
-            logging.info(f'Mask saved to {out_filename}')
+            out_prefix = os.path.splitext(out_files[i])[0]
+            save_probability_map(probs, out_prefix)
+            logging.info(f'Probability maps saved to {out_prefix}_classX_prob.png')
 
         if args.viz:
-            logging.info(f'Visualizing results for image {filename}, close to continue...')
-            plot_img_and_mask(img, mask)
+            logging.info(f'Visualizing Class 1 probability map for image {filename}, close to continue...')
+            if probs.shape[0] > 1:
+                class_index = 1
+            else:
+                class_index = 0  # falls nur 1 Klasse existiert (Binary Case)
+
+            #probs[class_index]=cv2.adaptiveThreshold((probs[class_index]*255).astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+
+            plt.imshow(probs[class_index], cmap='viridis')
+            plt.title(f'Class {class_index} Probability Map')
+            plt.colorbar()
+            plt.axis('off')
+            plt.show()
